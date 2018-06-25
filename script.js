a52bf76c974d079be227bdc3759932008b45a6c0
Ezekiel Williams <broncoscorpio@gmail.com>
2018-06-25 11:32:25 -0600

    Add sliding over-menu functionality. Next is buttons.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let overMenu, overMenuWidth;
+
 $(document).ready(() => {
-    
+    overMenu = $("#overMenu");
+    overMenuWidth = parseInt(overMenu.css("width"));
+    overMenu.css("height", window.innerHeight);
+    overMenu.css("left", -overMenuWidth);
 });
+
+function toggleMenu() {
+    let w = parseInt(overMenu.css("left"));
+
+    if (w !== 0) {
+        overMenu.css("left", 0);
+    }
+    else {
+        overMenu.css("left", -overMenuWidth);
+    }
+}
